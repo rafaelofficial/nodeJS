dbc14f22fb4ff062fee0d870ea3364e56b2c7af0
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-09-25 23:42:01 -0300

    added and used lib chalk

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
 console.log('Hello World');
-console.log('Are generated received go to of a object [fileList](https://developer.mozialla.org/pt-BR/docs/Web/API/FileList) which is returned as selection result, by user, of files often of elements [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), to go of object [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API');
-console.log('Are generated received go to of a object [fileList](https://developer.mozialla.org/pt-BR/docs/Web/API/FileList) which is returned as selection result, by user, of files often of elements [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), to go of object [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API');
+console.log(chalk.bgRedBright('Are generated received go to of a object [fileList](https://developer.mozialla.org/pt-BR/docs/Web/API/FileList) which is returned as selection result, by user, of files often of elements [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), to go of object [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API'));
+console.log(chalk.blue.bgWhite.bold('Are generated received go to of a object [fileList](https://developer.mozialla.org/pt-BR/docs/Web/API/FileList)) which is returned as selection result, by user, of files often of elements [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), to go of object [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API'));
